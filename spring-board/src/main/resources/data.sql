7b8309017247822951511e23828519f2e4aaa36c
alpha99k <alpha99k.codes@gmail.com>
2026-07-21 16:09:07 +0900

    회원 기능 과제 예제 추가

---
+++ spring-board/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO post2 (title, content, author, secret, created_at) VALUES ('1번 게시글', '1번 게시글 내용입니다.', '하루', true, CURRENT_TIMESTAMP);
+INSERT INTO post2 (title, content, author, secret, created_at) VALUES ('2번 게시글', '2번 게시글 내용입니다.', '나무', false, CURRENT_TIMESTAMP);
+INSERT INTO member2 (username, password, email, created_at) VALUES ('haru', 'pass1', 'user1@example.com', CURRENT_TIMESTAMP);
+INSERT INTO member2 (username, password, email, created_at) VALUES ('namu', 'pass2', 'user2@example.com', CURRENT_TIMESTAMP);
